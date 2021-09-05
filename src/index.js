9a0a5c9bd81e2ef4bf24d712d2b389addcbea6a1
Jesus Berrio <berriojesus122@gmail.com>
2021-09-05 15:21:46 -0500

    express añadido para que no explote

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 const dotenv = require('dotenv')
+import express from 'express'
 dotenv.config()
+
+
+const app = express()
+app.listen(3000, function () {
+    console.log(`http://localhost:${3000}`)
+  })
 require('./database')
 require('./tasks/deleteUser.js')
